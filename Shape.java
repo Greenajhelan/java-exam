1e7fea20dc10cd74178ccfda4dd4e923c38cc321
Greena j Helan <84068025+Greenajhelan@users.noreply.github.com>
2021-10-06 16:25:44 +0530

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+class Shape
+{
+public static void main(string arg[]);
+void calculate(int a,int b,char ch)
+{
+if(ch='r')
+int a=2*(1+b);
+Scanner sc=new Scanner(system.in);
+System.out.println("the rectangle" +a);
+Scanner a=stringInt;
+}
+void calculate(int a,int b )
+{
+if(ch='s')
+int b=(4*a);
+System.out.println("the square" +b);
+Scanner a=stringInt;
+}
+void calculate(int a)
+{
+if(ch='c')
+int a=2*3.14;
+System.out.println("the circle" +a);
+Scanner a=stringInt;
+}
+public class overload
+{
+int s,r,c,n=10;
+{
+ov.rectangle(r,6);
+ov.square(s,5);
+ov.circle(c,2);
+}
+}
+}
---
+++ Sum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class sum
+{
+public static void main(string arrg[]);
+int a,b,c;
+{
+Scanner sc=new Scanner(system.in);
+System.out.println("enter the number:");
+Scanner a=new Scanner(system.in);
+Scanner num=StringInt;
+Scanner b=new Scanner(system.in);
+Scanner num=StringInt;
+System.out.println("result"+a +b);
+System.out.println("even number"+c);
+}
+}
